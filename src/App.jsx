570b232a43bb51ca5aadd505d7d37d8c24231f08
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-02 15:06:39 +0530

    added card

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useEffect } from 'react'
 import './App.css';
 import SeacrhIcon from './search.svg';
 
-const API_URL = "http://www.omdbapi.com?apikey=557d4875";
+const API_URL = "http://www.omdbapi.com/?apikey=557d4875";
 
 function App() {
   // As soon as components load we need to fetch the data for that we will use useEffect 
 
   const searchMovies = async(title) => {
-    const response = await fetch(`${API_URL}$s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     console.log(data.Search);
