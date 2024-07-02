d87d8416d5b43ca93f12595fffb20321630cc257
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-02 14:55:08 +0530

    css and api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { useEffect } from 'react'
+import './App.css';
+import SeacrhIcon from './search.svg';
 
+const API_URL = "http://www.omdbapi.com?apikey=557d4875";
 
 function App() {
+  // As soon as components load we need to fetch the data for that we will use useEffect 
 
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}$s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('superman');
+  }, [])
   return (
-    <>
+    <> 
       <div>
         <h1>App</h1>
       </div>
